aac6d9ffeb1cc0f057ead1985af302bbd230f2de
JosueAlex <135286068+JosueAlex@users.noreply.github.com>
2023-06-08 15:39:49 -0600

    Update studentbd.sql

--- studentbd.sql
+++ studentbd.sql
@@ -1,1 +1,3 @@
 use student_hub;
+insert into registro_de_usuarios values (id, nombre, usuario, correo, contraseña, fecha_de_nacimiento) values 
+('11','Daniela Martinez','DaniiM23','Dani@example.com','lolo1234','23 de Julio de 2001');
